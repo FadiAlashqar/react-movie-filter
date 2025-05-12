7ee4d53cbfc14b5c18d40e75d82ed4a63620f35d
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-05-12 17:45:41 +0200

    [UPD] list done

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,34 @@ import { useState } from 'react'
 
 function App() {
   const initialFilms = [
-    { title: 'Inception', genre: 'Fantascienza' },
-    { title: 'Il Padrino', genre: 'Thriller' },
-    { title: 'Titanic', genre: 'Romantico' },
-    { title: 'Batman', genre: 'Azione' },
-    { title: 'Interstellar', genre: 'Fantascienza' },
-    { title: 'Pulp Fiction', genre: 'Thriller' },
+    { id: 1, title: 'Inception', genre: 'Fantascienza' },
+    { id: 2, title: 'Il Padrino', genre: 'Thriller' },
+    { id: 3, title: 'Titanic', genre: 'Romantico' },
+    { id: 4, title: 'Batman', genre: 'Azione' },
+    { id: 5, title: 'Interstellar', genre: 'Fantascienza' },
+    { id: 6, title: 'Pulp Fiction', genre: 'Thriller' },
   ]
   return (
     <>
-      <h1>HELLO WORLD</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 mt-4">
+            <h1>FILMS</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <ul className="list-group">
+              {initialFilms.map((film) => {
+                return <li key={film.id} className="list-group-item">
+                  <div>TITOLO : {film.title}</div>
+                  <div>GENERE : {film.genre}</div>
+                </li>
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
